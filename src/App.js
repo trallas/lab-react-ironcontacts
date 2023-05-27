499a170a9d8be97e3fa4577aa5346d03517c003e
Xavi <xaviertralleroguillot@gmail.com>
2023-05-27 14:31:09 +0200

    lab solved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import "./contacts.json"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [contacts, setContacts] = useState(contactsData.slice(0, 5));
+const [remainingContacts, setRemainingContacts] = useState(contactsData.slice(5));
+<><button onClick={addRandomContact}>Add Random Contact</button><button onClick={resetContacts}>Sort by Popularity</button><button onClick={showAllContacts}>Sort by Name</button></>
+  function App() {
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Won an Oscar</th>
+              <th>Won an Emmy</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts.map((contact) => (
+              <tr key={contact.id}>
+                <td>
+                  <img src={contact.pictureUrl} alt={contact.name} />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+                <td>{contact.wonOscar}</td>
+                <td>{contact.wonEmi}</td>
+                <button onClick={addRandomContact}>Add Random Contact</button>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+    
 }
-
 export default App;
